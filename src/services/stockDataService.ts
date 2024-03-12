8bbae39e8019781efeca4296664806deef660279
Joris_Dev <61516195+JorisSDev@users.noreply.github.com>
2024-03-12 15:12:33 +0200

    Add data parsing from API response #ID5.1

--- src/services/stockDataService.ts
+++ src/services/stockDataService.ts
@@ -1,35 +1,35 @@
 // example service class, modify if needed, otherwise extend
-
+import { v4 as uuidv4 } from 'uuid';
 import axios from "axios";
 import { StockDataMapper } from "../mappers/stockDataMapper";
 import { IStockDataRepository } from "../repositories/stockRepository";
 
 export class StockDataService {
-    private repository: IStockDataRepository;
+  private repository: IStockDataRepository;
 
-    constructor(stockDataRepo: IStockDataRepository) {
-        this.repository = stockDataRepo;
-    }
+  constructor(stockDataRepo: IStockDataRepository) {
+    this.repository = stockDataRepo;
+  }
 
-    async fetchDataAndStore(): Promise<void> {
-        try {
-            const response = await axios.get("https://api.thirdparty.com/stock/data");
-            const stockData = StockDataMapper.toStockData(response.data);
-            await this.repository.save(stockData);
-        } catch (error) {
-            console.error("Error fetching and storing stock data:", error);
-            // Handle error appropriately
-        }
+  async fetchDataAndStore(): Promise<void> {
+    try {
+      const response = await axios.get("https://api.thirdparty.com/stock/data");
+      const stockData = StockDataMapper.toStockData(response.data, "AMZN", uuidv4());
+      await this.repository.save(stockData);
+    } catch (error) {
+      console.error("Error fetching and storing stock data:", error);
+      // Handle error appropriately
     }
+  }
 
-    async giveDataForStock(stockReqDTO: any): Promise<any> {
-      try {
-        // get data from repo
-        // return await this.repository.getDataForStock(stockReqDTO);
-      }
-      catch (error) {
-        console.error("Error getting stock data:", error);
-        // Handle error appropriately
-      }
+  async giveDataForStock(stockReqDTO: any): Promise<any> {
+    try {
+      // get data from repo
+      // return await this.repository.getDataForStock(stockReqDTO);
     }
+    catch (error) {
+      console.error("Error getting stock data:", error);
+      // Handle error appropriately
+    }
+  }
 }
