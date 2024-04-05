f86fc680965dfd04ce7c76fdf38100eb55462417
Gytismar <25962211+Gytismar@users.noreply.github.com>
2024-04-06 00:00:36 +0300

    Add tests for save method in StockDataRepository #ID-50

--- tests/stockRepository.test.ts
+++ tests/stockRepository.test.ts
@@ -1,3 +1,6 @@
+import { StockDataRepository } from '../src/repositories/stockRepository';
+import { Container } from '@azure/cosmos';
+import { mockDeep } from 'jest-mock-extended';
 import { StockDataMapper } from '../src/mappers/stockDataMapper';
 
 jest.mock('../src/mappers/stockDataMapper', () => ({
@@ -24,3 +27,66 @@ jest.mock('../src/mappers/stockDataMapper', () => ({
     })
   }
 }));
+
+describe('StockDataRepository', () => {
+    let containerMock: Container;
+    let stockDataRepository: StockDataRepository;
+    let consoleErrorSpy: jest.SpyInstance;
+  
+    beforeAll(() => {
+
+      containerMock = mockDeep<Container>();
+      stockDataRepository = new StockDataRepository(containerMock);
+      consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+    });
+  
+    afterAll(() => {
+      consoleErrorSpy.mockRestore();
+    });
+  
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+  describe('save', () => {
+    it('should upsert stock data successfully', async () => {
+      const stockData = {
+        id: 'mock-id',
+        symbol: 'TEST',
+        timestamp: new Date('2023-04-03T12:00:00Z'),
+        volume: 1000,
+        high: 10.5,
+        low: 9.8,
+        close: 10.2,
+        open: 10.0,
+      };
+
+      await stockDataRepository.save(stockData);
+
+      expect(containerMock.items.upsert).toHaveBeenCalledWith({
+        id: 'mock-id',
+        symbol: 'TEST',
+        timestamp: '2023-04-03T12:00:00.000Z',
+        volume: 1000,
+        high: 10.5,
+        low: 9.8,
+        close: 10.2,
+        open: 10.0,
+      });
+      expect(StockDataMapper.stockDataToPersistence).toHaveBeenCalledWith(stockData);
+    });
+
+    it('should log an error if saving stock data fails', async () => {
+        // Arrange
+        const error = new Error('Test Error');
+        containerMock.items.upsert = jest.fn().mockRejectedValueOnce(error);
+  
+        // Act
+        await stockDataRepository.save({} as any);
+  
+        // Assess
+        expect(consoleErrorSpy).toHaveBeenCalledWith("Error saving stock data:", error);
+      });
+  });
+});
+
