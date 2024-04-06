a0b0d6f11fa44b6750586af9b2b4c81a1747f76f
Gediminas Šeibokas <gedisaiba@gmail.com>
2024-04-06 22:05:01 +0300

    Add integration test for data going from service to repository #ID-57

--- tests/stockDataServiceAndRepository.int.test.ts
+++ tests/stockDataServiceAndRepository.int.test.ts
@@ -12,6 +12,7 @@ jest.mock('../src/cosmosClientInstance', () => {
     return {
         container: {
             items: {
+                upsert: jest.fn(),
             },
         },
     };
@@ -29,4 +30,40 @@ describe('Integration between StockDataService and StockDataRepository', () => {
         repository = new StockDataRepository(container);
         service = new StockDataService(repository);
     });
+
+    it('Fetches stock data from API and saves it in the database', async () => {
+        const symbol = 'AMZN';
+        // Arrange
+        const fakeStockData = {
+            symbol: symbol,
+            t: Math.floor(new Date().getTime() / 1000),
+            v: 1000,
+            o: 100,
+            h: 110,
+            l: 90,
+            c: 105,
+        };
+
+        mockAxios.get.mockResolvedValue({ data: { data: [fakeStockData] } });
+
+        (StockDataMapper.toStockData as jest.Mock).mockReturnValue({
+            id: '1',
+            symbol: fakeStockData.symbol,
+            timestamp: new Date(fakeStockData.t * 1000),
+            volume: fakeStockData.v,
+            open: fakeStockData.o,
+            high: fakeStockData.h,
+            low: fakeStockData.l,
+            close: fakeStockData.c,
+        });
+
+        // Act
+        const result = await service.fetchStockData('AMZN');
+
+        // Assert
+        expect(StockDataMapper.toStockData).toHaveBeenCalledTimes(1);
+        expect(container.items.upsert).toHaveBeenCalled();
+        expect(result.symbol).toEqual(symbol);
+    });
+
 });
