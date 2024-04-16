836c19a5c9887f63ed4c8f601f1349f3c871b97f
winqus <67459222+winqus@users.noreply.github.com>
2024-04-16 17:19:40 +0300

    Refactor cache implementation and add environment variables for cache size and expiration time #ID-68

--- src/services/cache.ts
+++ src/services/cache.ts
@@ -1,26 +1,73 @@
 interface CacheEntry<T> {
   value: T;
   expiresAt: number;
+  lastAccessed: number;
 }
 
 export class CacheService<T> {
-  private cache: Map<string, CacheEntry<T>> = new Map<string, CacheEntry<T>>();
+  private _cache: Map<string, CacheEntry<T>> = new Map<string, CacheEntry<T>>();
+  private _maxEntries: number;
+  private _defaultExpirationInMs: number;
+
+  constructor(maxEntries: number = 100, defaultExpirationInMinutes: number = 10) { 
+    this._maxEntries = maxEntries;
+    this._defaultExpirationInMs = this.minutesToMilliseconds(defaultExpirationInMinutes);
+  }
 
   async get(key: string): Promise<T | null> {
-    const entry = this.cache.get(key);
+    const entry = this._cache.get(key);
     if (entry && entry.expiresAt > Date.now()) {
+      entry.lastAccessed = Date.now();
+
       return entry.value;
     }
+    
+    this._cache.delete(key);
+
     return null;
   }
 
-  async set(key: string, value: T, expirationTimeInMs: number = 1 * 60 * 60 * 1000): Promise<void> {
+  async set(key: string, value: T, expirationTimeInMs: number = this._defaultExpirationInMs): Promise<void> {
+    if (this._cache.size >= this._maxEntries) {
+      this.evictExpiredEntries();
+    }
+    if (this._cache.size >= this._maxEntries) {
+      this.evictLRUEntry();
+    }
     const expiresAt = Date.now() + expirationTimeInMs;
-    this.cache.set(key, { value, expiresAt });
+    this._cache.set(key, { value, expiresAt, lastAccessed: Date.now() });
   }
 
   async clear(): Promise<void> {
-    this.cache.clear();
+    this._cache.clear();
+  }
+
+  private evictExpiredEntries(): void {
+    const now = Date.now();
+    for (let [key, entry] of this._cache) {
+      if (entry.expiresAt <= now) {
+        this._cache.delete(key);
+      }
+    }
+  }
+
+
+  private evictLRUEntry(): void {
+    const leastRecentlyUsed = Array.from(this._cache.entries()).reduce((lru, entry) => {
+      if (!lru || entry[1].lastAccessed < lru[1].lastAccessed) {
+        return entry;
+      }
+
+      return lru;
+    }, null as [string, CacheEntry<T>] | null);
+
+    if (leastRecentlyUsed) {
+      this._cache.delete(leastRecentlyUsed[0]);
+    }
+  }
+
+  minutesToMilliseconds(minutes: number): number {
+    return minutes * 60 * 1000;
   }
 }
 
--- src/stockDataResponseCacheInstance.ts
+++ src/stockDataResponseCacheInstance.ts
@@ -1,3 +1,6 @@
 import { CacheService } from "./services/cache";
 
-export const cacheInstance = new CacheService<string>();
+const cacheSize = process.env.CACHE_SIZE ? parseInt(process.env.CACHE_SIZE) : 1000;
+const cacheExpirationInMinutes = process.env.CACHE_EXPIRATION_IN_MINUTES ? parseInt(process.env.CACHE_EXPIRATION_IN_MINUTES) : 10;
+
+export const cacheInstance = new CacheService<string>(cacheSize, cacheExpirationInMinutes);
